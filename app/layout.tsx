7e883f44e998e880806fed1f73d1571b5b95bd44
ImMalloy <lefehanims@gmail.com>
2026-06-02 12:16:51 -0700

    fix uri

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000"
-  ),
+  metadataBase: new URL("https://malloy.vercel.app"),
   title: "ImMalloy",
   description: ":)",
   openGraph: {
