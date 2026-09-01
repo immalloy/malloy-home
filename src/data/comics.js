22de94a8448ea2e40b9e3d4199cf6736668a45b8
ImMalloy <lefehanims@gmail.com>
2026-09-01 16:18:19 -0600

    Spanish Support Hotfix pre upload

--- src/data/comics.js
+++ src/data/comics.js
@@ -2,10 +2,7 @@ export const comics = [
   {
     title: "Pretty, Isn’t It?",
     description: "B01 shows Bumyx something beautiful.",
-    titleEs: "¿No es hermoso?",
-    descriptionEs: "B01 le muestra algo hermoso a Bumyx.",
     src: "/assets/comics/1.webp",
     alt: "B01 shows Bumyx a beautiful scene, and Bumyx tears up.",
-    altEs: "B01 le muestra un paisaje hermoso a Bumyx, y Bumyx se emociona.",
   },
 ];
