8747a37d8591b3642b65da3b3f3eab8b16ae304a
ImMalloy <lefehanims@gmail.com>
2026-09-20 21:59:09 -0600

    remove translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://immalloy.com",
-  output: "static",
-  i18n: {
-    locales: ["en", "es"],
-    defaultLocale: "en",
-    routing: {
-      prefixDefaultLocale: false,
-      redirectToDefaultLocale: false,
-      fallbackType: "redirect"
-    }
-  }
+  output: "static"
 });
